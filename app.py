321d26d54079afa3babcf9084f27e3aa02729144
LEE950109 <72004476+LEE950109@users.noreply.github.com>
2024-11-21 17:13:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
+import boto3
 import json
+
 def lambda_handler(event, context):
-    """
-    HTTP 요청을 처리하는 AWS lambda 함수
-    """
-    # print("Event: ", event)
-    #
-    # response = {
-    #     "statusCode": 200,
-    #     "headers":{
-    #         "Content-Type": "application/json"
-    #     },
-    #     "body": json.dumps({
-    #         "message": "Hello from AWS lambda!",
-    #         "input": event
-    #     })
-    # }
-    return{
-        "statusCode": 200,
-        "body": "Hello, AWS Lambda!!!"
-    }
+    # AWS CodePipeline 클라이언트 생성
+    codepipeline = boto3.client('codepipeline')
+
+    # CodePipeline 이벤트에서 Job ID 추출
+    job_id = event['CodePipeline.job']['id']
+
+    try:
+        # Lambda 작업 로직
+        result = {
+            'statusCode': 200,
+            'body': json.dumps('Hello from Lambda!')
+        }
+
+        # 작업 성공 알림
+        codepipeline.put_job_success_result(jobId=job_id)
+
+        return result
+    except Exception as e:
+        # 작업 실패 알림
+        codepipeline.put_job_failure_result(
+            jobId=job_id,
+            failureDetails={
+                'type': 'JobFailed',
+                'message': str(e)
+            }
+        )
+        raise e
