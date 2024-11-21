1ad7796f549dc96d09e05bd06ce80c38cf692c41
LEE950109 <erioer95@konkuk.ac.kr>
2024-11-21 14:00:35 +0900

    initial commit for lambda deployment

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+def lambda_handler(event, context):
+    """
+    HTTP 요청을 처리하는 AWS lambda 함수
+    """
+    print("Event: ", event)
+
+    response = {
+        "statusCode": 200,
+        "headers":{
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Hello from AWS lambda!",
+            "input": event
+        })
+    }
