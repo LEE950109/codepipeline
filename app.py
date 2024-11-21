c400b15eb823ad3763524885697cf9de68ba579a
LEE950109 <erioer95@konkuk.ac.kr>
2024-11-21 15:02:34 +0900

    update lambda function message

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ def lambda_handler(event, context):
     """
     HTTP 요청을 처리하는 AWS lambda 함수
     """
-    print("Event: ", event)
-
-    response = {
+    # print("Event: ", event)
+    #
+    # response = {
+    #     "statusCode": 200,
+    #     "headers":{
+    #         "Content-Type": "application/json"
+    #     },
+    #     "body": json.dumps({
+    #         "message": "Hello from AWS lambda!",
+    #         "input": event
+    #     })
+    # }
+    return{
         "statusCode": 200,
-        "headers":{
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "message": "Hello from AWS lambda!",
-            "input": event
-        })
+        "body": "Hello, AWS Lambda!"
     }
